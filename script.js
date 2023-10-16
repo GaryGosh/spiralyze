b72986e52dcc9018bd630748684a9307b09499a7
Neeraj TK <64883030+GaryGosh@users.noreply.github.com>
2023-10-16 11:33:03 +0530

    carousel added

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
-console.log("hihi");
+"use strict";
+// Select all slides
+const slides = document.querySelectorAll(".slide");
+
+// Select all indicators
+const indicators = document.querySelectorAll(".indicator");
+
+// loop through slides and set each slide's translateX
+slides.forEach((slide, indx) => {
+  slide.style.transform = `translateX(${indx * 100}%)`;
+});
+
+// current slide counter
+let curSlide = 0;
+// maximum number of slides
+let maxSlide = slides.length - 1;
+
+// Function to update the active indicator
+function updateIndicators() {
+  indicators.forEach((indicator, indx) => {
+    if (indx === curSlide) {
+      indicator.classList.add("active");
+    } else {
+      indicator.classList.remove("active");
+    }
+  });
+}
+
+// Function to handle slide navigation
+function navigateSlide(direction) {
+  if (direction === "next") {
+    curSlide = (curSlide + 1) % slides.length;
+  } else {
+    curSlide = (curSlide - 1 + slides.length) % slides.length;
+  }
+
+  // Move the slides
+  slides.forEach((slide, indx) => {
+    slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
+  });
+
+  // Update the active indicator
+  updateIndicators();
+}
+
+// Add event listener for next button
+const nextSlide = document.querySelector(".btn-next");
+nextSlide.addEventListener("click", () => {
+  navigateSlide("next");
+});
+
+// Add event listener for previous button
+const prevSlide = document.querySelector(".btn-prev");
+prevSlide.addEventListener("click", () => {
+  navigateSlide("prev");
+});
+
+// Initialize the indicators
+updateIndicators();
