8fda5a70d26e1817368a7a17bebdc4ba353e2c7b
Neeraj TK <64883030+GaryGosh@users.noreply.github.com>
2023-10-18 00:25:32 +0530

    submit form added

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ updateIndicators();
 const video = document.querySelector(".main-video");
 const playBtn = document.querySelector(".svg-container");
 
-// Function to toggle play/pause
+// to toggle play/pause
 function togglePlay() {
   if (video.paused) {
     video.play();
@@ -70,11 +70,59 @@ function togglePlay() {
   }
 }
 
-// Add a click event listener to the play button
+// event listener for the play button
 playBtn.addEventListener("click", togglePlay);
 
 document.addEventListener("DOMContentLoaded", function () {
   const currentYear = new Date().getFullYear();
   const yearSpan = document.getElementById("current-year");
   yearSpan.textContent = currentYear;
+});
+
+// contact detail form
+const form = document.getElementById("myForm");
+
+form.addEventListener("submit", function (e) {
+  const firstName = document.getElementById("firstName");
+  const lastName = document.getElementById("lastName");
+  const email = document.getElementById("email");
+  const company = document.getElementById("company");
+  const country = document.getElementById("country");
+
+  const firstNameError = document.getElementById("firstName-error");
+  const lastNameError = document.getElementById("lastName-error");
+  const emailError = document.getElementById("email-error");
+  const companyError = document.getElementById("company-error");
+  const countryError = document.getElementById("country-error");
+
+  firstNameError.textContent = "";
+  lastNameError.textContent = "";
+  emailError.textContent = "";
+  companyError.textContent = "";
+  countryError.textContent = "";
+
+  if (!firstName.value) {
+    e.preventDefault();
+    firstNameError.textContent = "First Name cannot be empty";
+  }
+
+  if (!lastName.value) {
+    e.preventDefault();
+    lastNameError.textContent = "Last Name cannot be empty";
+  }
+
+  if (!email.value) {
+    e.preventDefault();
+    emailError.textContent = "Email cannot be empty";
+  }
+
+  if (!company.value) {
+    e.preventDefault();
+    companyError.textContent = "Company cannot be empty";
+  }
+
+  if (!country.value) {
+    e.preventDefault();
+    countryError.textContent = "Please select a country";
+  }
 });
